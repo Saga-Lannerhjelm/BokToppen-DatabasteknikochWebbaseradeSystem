fe301af6a6532fe15d8e9b7f1ed3c1c76ee268d4
Saga-Lannerhjelm <saga@lannerhjelm.se>
2024-10-01 08:40:01 +0200

    change name of variable

--- BokToppen/Controllers/LoginController.cs
+++ BokToppen/Controllers/LoginController.cs
@@ -24,8 +24,8 @@ namespace BokToppen.Controllers
         {
             if (ModelState.IsValid)
             {
-                UserMethod um = new UserMethod();
-                int existingUserId = um.GetUser(user, out string error);
+                UserMethod unserMethod = new UserMethod();
+                int existingUserId = unserMethod.GetUser(user, out string error);
                 if(existingUserId != 0){
                     // Sätter session variabel som visar att användaren är "inloggad"
                     HttpContext.Session.SetString("UserId", existingUserId.ToString());
@@ -45,17 +45,6 @@ namespace BokToppen.Controllers
         {
             HttpContext.Session.Remove("UserId");
             return RedirectToAction("Index", "Login");
-        }
-
-        // Implementerad baserat på kodexempel från https://www.thatsoftwaredude.com/content/6218/how-to-encrypt-passwords-using-sha-256-in-c-and-net
-        private byte[] CalculateSHA256(string password)
-        {
-            SHA256 sha256 = SHA256.Create();
-            byte[] hashValue;
-            UTF8Encoding objUtf8 = new UTF8Encoding();
-            hashValue = sha256.ComputeHash(objUtf8.GetBytes(password));
-
-            return hashValue;
         }
 
 
